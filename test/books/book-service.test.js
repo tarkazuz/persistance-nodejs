ea17a77116da5ce9814681046b4918618009cc60
Marco Spiess <marco.spiess01@sap.com>
2020-08-04 08:56:58 +0200

    Add imports to book-service-test and use arrow functions consistently

--- test/books/book-service.test.js
+++ test/books/book-service.test.js
@@ -1,15 +1,17 @@
+const assert = require('assert');
+const BookService = require('../../src/books/book-service');
 const startPostgresContainer = require('../start-pg-container');
 
 describe('BookService', () => {
 
     let container;
 
-    before(async function () {
+    before(async () => {
         this.timeout(0);
         container = await startPostgresContainer();
     });
 
-    after(async function () {
+    after(async () => {
         this.timeout(0);
         await container.stop();
     });
