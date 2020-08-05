591fa3fdcce342ea66ec2645e841765e7186daab
Marco Spiess <marco.spiess01@sap.com>
2020-08-05 09:45:03 +0200

    Create db instance in main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 const Server = require('./server');
 const BookRouter = require('./books/book-router');
 const BookService = require('./books/book-service');
+const DB = require('./db');
 
 (async function () {
-    const bookService = new BookService();
+    const db = new DB();
+    const bookService = new BookService(db);
     const bookRouter = new BookRouter(bookService);
     new Server(bookRouter);
 })();
