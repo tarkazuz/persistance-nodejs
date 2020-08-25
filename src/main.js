fe3848dafce766fc2586d6ccc51dce04b3a82860
Marco Spiess <marco.spiess01@sap.com>
2020-08-25 16:44:42 +0200

    remove GUI

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
 'use strict';
 
-const Server = require('./server');
-const BookRouter = require('./books/book-router');
-const BookService = require('./books/book-service');
-const DB = require('./db');
-
-(async function () {
-    const db = new DB();
-    const bookService = new BookService(db);
-    const bookRouter = new BookRouter(bookService);
-    new Server(bookRouter);
-})();
+console.log('Nothing to do here... Try `npm test`!');
