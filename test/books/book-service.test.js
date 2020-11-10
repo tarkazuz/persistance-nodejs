5b9869ab02360cdd306f95f6846d2a6489371a9f
C5309084 <sueleyman.bilgi@sap.com>
2020-11-11 00:43:59 +0100

    move todo comment

--- test/books/book-service.test.js
+++ test/books/book-service.test.js
@@ -3,8 +3,8 @@ const NotImplementedError = require('../../src/not-implemented-error');
 
 describe('BookService', function () {
 
+    // TODO: replace this object with the Pool of node-postgres driver at ex 1.2
     const pool = {
-        // TODO: replace this object with the Pool of node-postgres driver at ex 1.2
         query: async () => {
             throw new NotImplementedError();
         }
