eaece6550d92a38fd2d28678f526c8ac1e9896c0
C5309084 <sueleyman.bilgi@sap.com>
2020-11-11 00:10:12 +0100

    try new starting point

--- test/books/book-service.test.js
+++ test/books/book-service.test.js
@@ -3,7 +3,12 @@ const BookService = require('../../src/books/book-service');
 
 describe('BookService', function () {
 
-    const pool = undefined;
+    const pool = {
+        // TODO: replace this object with the Pool of node-postgres driver at ex 1.2
+        query = async () => {
+            return undefined;
+        }
+    };
 
     it('should connect to the database', async function() {
         assert.fail('TODO');
