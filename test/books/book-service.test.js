6f5321495be2aea0005fb46dc0e6dcc87443fec7
C5309084 <sueleyman.bilgi@sap.com>
2020-11-13 14:03:20 +0100

    remove fake pool object

--- test/books/book-service.test.js
+++ test/books/book-service.test.js
@@ -1,14 +1,6 @@
 const assert = require('assert');
-const NotImplementedError = require('../../src/not-implemented-error');
 
 describe('BookService', function () {
-
-    // TODO: replace this object with the Pool of node-postgres driver at ex 1.3
-    const pool = {
-        query: async () => {
-            throw new NotImplementedError();
-        }
-    };
 
     it('should connect to the database', async function() {
         assert.fail('TODO');
