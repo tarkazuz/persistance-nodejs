31c8b40708380ce4a74c496b3fbd4c7df298ebc8
C5309084 <sueleyman.bilgi@sap.com>
2020-11-24 17:01:41 +0100

    fix constraint creation in schema

--- src/schema.js
+++ src/schema.js
@@ -1,5 +1,6 @@
 module.exports = {
     CREATE_SCHEMA_SQL: `CREATE TABLE IF NOT EXISTS "books" (id SERIAL PRIMARY KEY, title VARCHAR(255), author VARCHAR(255));
+                        ALTER TABLE "books" DROP CONSTRAINT IF EXISTS title_author_uniqueness;
                         ALTER TABLE "books" ADD CONSTRAINT title_author_uniqueness UNIQUE(title, author)`,
     DROP_SCHEMA_SQL: `DROP TABLE IF EXISTS "books";`
 };
