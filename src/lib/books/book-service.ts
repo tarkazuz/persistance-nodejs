58ba76feb2f1548346bd8fabff05392ac30a5212
Jacques Huss <jacques.huss@sap.com>
2023-02-17 14:41:03 +0100

    replaced empty implementation with interface

--- src/lib/books/book-service.ts
+++ src/lib/books/book-service.ts
@@ -1,20 +1,8 @@
-import { Book } from '../books/book'
-import NotImplementedError from '../not-implemented-error'
+import { Book } from './book'
 
-export default class BookService {
-  static UNIQUE_VIOLATION_ERROR_CODE = '23505'
-
-  constructor() { }
-
-  async getAllBooks ():Promise<Book[]> {
-    throw new NotImplementedError()
-  }
-
-  async addBook(book:Book) {
-    throw new NotImplementedError()
-  }
-
-  async getBookByTitle() {
-    throw new NotImplementedError()
-  }
+interface BookStorage{
+  getAllBooks ():Promise<Book[]>;
+  addBook(book:Book):Book;
+  getBookByTitle(title:String):Book;
 }
+
